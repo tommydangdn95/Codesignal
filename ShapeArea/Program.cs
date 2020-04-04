961da1a4ccfee47bb32f792c1da399e5a0c02cb3
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-04 11:38:11 +0700

    Shape Area
    Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.

---
+++ ShapeArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(shapeArea(4));
+        }
+
+        static int shapeArea(int n)
+        {
+            return (n * n) + (n - 1) * (n - 1);
+        }
+
+    }
+}
