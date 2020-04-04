75277e956a2d5ac735fc12dbb2e10b4ab416e697
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-04 11:18:30 +0700

    AdjacentElements

---
+++ AdjacentElementsProduct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AdjacentElementsProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 3, 6, -2, -5, 7, 3 };
+            Console.WriteLine(adjacentElementsProduct(array));
+        }
+        static int adjacentElementsProduct(int[] inputArray)
+        {
+            int max = inputArray[0] * inputArray[1];
+            for(int i = 0; i< inputArray.Length - 1; i++)
+            {
+                var produce = inputArray[i] * inputArray[i + 1];
+                if (produce > max)
+                    max = produce;
+            }
+
+            return max;
+        }
+
+    }
+}
