09f8e32c22b2ee716413f2a7b6ee312ca10d0318
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-08 12:53:24 +0700

    Make Array Consecutive

---
+++ MakeArrayConsecutive/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MakeArrayConsecutive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArr = new int[] { 0, 3 };
+            var count = makeArrayConsecutive2(testArr);
+            Console.WriteLine(count);
+        }
+
+        static int makeArrayConsecutive2(int[] statues)
+        {
+            var count = 0;
+            Array.Sort(statues);
+            for(int i= 0;i<statues.Length - 1; i++)
+            {
+                if(statues[i+1] - statues[i] > 1)
+                {
+                    count += statues[i + 1] - statues[i] - 1;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
